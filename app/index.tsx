4bb99c4cd3c78459178f3731562cf679d0d6f0fc
Adrian <adrianconstainb@gmail.com>
2025-03-25 22:05:13 -0500

    Login

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,7 @@
+import LoginScreen from "@/src/layouts/LoginScreen";
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text> buenas</Text>
-    </View>
-  );
+  return <LoginScreen />
+
 }
