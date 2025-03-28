47e9d6752be70b990ac7529c42ad9f49e821df9e
Adrian <adrianconstainb@gmail.com>
2025-03-27 21:57:22 -0500

    LoginScreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import LoginScreen from "@/src/layouts/LoginScreen";
-import { Text, View } from "react-native";
 
 export default function Index() {
-  return <LoginScreen />
+  return <LoginScreen />;
 
 }
